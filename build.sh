d5892040f43dff41aa0444920874150cd42587fa
Alexandre de Siqueira <5776022+alexdesiqueira@users.noreply.github.com>
2024-12-12 09:51:37 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,6 +15,9 @@ RELEASE="$(rpm -E %fedora)"
 grep -v '^#' /tmp/packages | xargs rpm-ostree install
 grep -v '^#' /tmp/emacs-ide.txt | xargs rpm-ostree install
 
+# Remove packages
+rpm-ostree override remove thunar firefox
+
 #### Example for enabling a System Unit File
 systemctl enable docker.socket
 systemctl enable podman.socket
