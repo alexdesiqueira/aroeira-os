423b471f55efbed089744e3880d9421973ad2454
alexdesiqueira <alex@tucanalabs.com>
2024-12-12 21:40:46 -0300

    Trying again, with AroeiraOS material

--- shell-scripts/build.sh
+++ shell-scripts/build.sh
@@ -4,6 +4,14 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-#### Example for enabling a System Unit File
-systemctl enable docker.socket
+
+# Install packages
+
+## RPMfusion repos are available by default in ublue main images
+## List of rpmfusion packages can be found here:
+## https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+
+cat /tmp/*.txt | xargs rpm-ostree install
+
+# Enabling a System Unit File
 systemctl enable podman.socket
